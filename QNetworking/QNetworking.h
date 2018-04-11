5a9466c5c4d24fd8f8fe873eeb464b68f733c505
ArifADS <arifads@gmail.com>
2018-04-11 13:27:35 +0200

    Initial Commit

---
+++ QNetworking/QNetworking.h
@@ -0,0 +1,19 @@
+//
+//  QNetworking.h
+//  QNetworking
+//
+//  Created by Arif De Sousa on 11/04/2018.
+//  Copyright © 2018 Quadram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QNetworking.
+FOUNDATION_EXPORT double QNetworkingVersionNumber;
+
+//! Project version string for QNetworking.
+FOUNDATION_EXPORT const unsigned char QNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QNetworking/PublicHeader.h>
+
+
